9e9f5123657b04a822927e5383727d407cd4cd55
Mak513 <osei.makangu@hotmail.com>
2021-11-22 10:58:05 +0100

    Osei

---
+++ index.js
@@ -0,0 +1,13 @@
+let pakete = 10;
+while (pakete > 0) {
+
+    if (pakete == 5) {
+        console.log("Ich mache eine Pause");
+    }
+    console.log("Paket ausgeliefert");
+    pakete--;
+
+    if (pakete == 0) {
+        console.log("Ich habe Feierabend");
+    }
+}
