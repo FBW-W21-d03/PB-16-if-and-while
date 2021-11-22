7a59c9d203bc93606cb31e9d40a200b98b27ccbe
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-11-22 11:18:13 +0100

    js

---
+++ index.js
@@ -0,0 +1,13 @@
+let Pakete =10; 
+while (Pakete>0){
+    Pakete--;
+    let Paketeminus ="Paket ausgeliefert";
+    console.log(Paketeminus,Pakete);
+    if(Pakete==5){
+    console.log("Ich mache eine Pause");
+    }
+};
+
+
+
+
