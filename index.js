77b4ee48fab15f1fe9ea46addd40690cd03c59e3
ShiinyFlakes <mago_elm@hotmail.com>
2021-11-22 11:06:48 +0100

    Mago

---
+++ index.js
@@ -0,0 +1,12 @@
+let x = 10; 
+
+while (x > 0) {
+    if(x == 5){
+        console.log("macht eine Pause");
+    }
+    console.log("paket ausgeleifert");
+    x--;
+    if(x == 0){
+        console.log("Schluss");
+    }
+}
