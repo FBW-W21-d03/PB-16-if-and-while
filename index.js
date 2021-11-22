a953148cad5c3afcc776809e496782f177f9d648
aisha2000-hub <aisha.noori2000@gmail.com>
2021-11-22 11:20:50 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,16 @@
+ let packet = 10;
+
+ while (packet > 0) {
+  
+ if (packet == 5) {
+    console.log("Ich mache eine Pause");
+    
+ }
+ console.log("Packet ausgelifert");
+ packet--;
+ if (packet == 0) {
+     console.log("Fertig")
+     
+ }
+
+ }
