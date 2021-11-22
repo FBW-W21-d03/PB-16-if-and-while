9d55f6277e4bc8e976b954efebc674dc81103b73
sprotte <metatron1972@live.de>
2021-11-22 11:13:29 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 let auto = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log ("packet ist ausgeliefert");
 for (auto = 0; auto < 5; auto++) {
@@ -8,3 +9,19 @@ if (auto > 5) {
 } else if (auto > 10) {
 } else {console.log("pause");
 }
+*/
+let paket = 10;
+let pause = "pause"
+let ausgeliefert = "paket ausgeliefert"
+
+while(paket <= 10){
+console.log(ausgeliefert);
+paket--;
+
+if(paket == 5){
+    console.log(pause);
+}
+
+if(paket == 0){ break; }
+}
+console.log("Fertig");
