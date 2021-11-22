b9b03023d1e0bbf87b3232f2de128d42bc897db9
sham912411 <shamalhakim91@gmail.com>
2021-11-22 11:28:53 +0100

    aufgabe

---
+++ paket.js
@@ -0,0 +1,12 @@
+let paket =10;
+
+while (paket > 0) { 
+paket--;
+
+console.log ( "Paket wird ausgeliefert")
+    
+    if (paket == 5)  {
+        console.log ( "Ich mache eine Pause" );
+    }
+    
+}
