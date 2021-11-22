ce6d5febd368353cacbf6d0e487fe41c445ef2e9
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-11-22 11:38:48 +0100

    ??? ich stehe auf dem schlauch..

---
+++ PB-16-if-and-while-enza.js
@@ -0,0 +1,16 @@
+//1.)
+let paket = 10;
+
+//2.)
+while
+ (paket > 0) {
+
+   if (paket == 5){console.log("kaffeepause");}
+   else{console.log("ist ausgeliefert");}
+
+  console.log(paket -1);
+  paket = paket -1;
+} 
+
+
+
