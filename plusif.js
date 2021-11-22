9d8e703f5e6641b22f0ad08b43c3a14383a40cac
slavablack <veaceslav.ciornii@magenta.de>
2021-11-22 11:15:01 +0100

    Packete geliefert

---
+++ plusif.js
@@ -0,0 +1,11 @@
+let packete=10;
+while (packete>0) {
+    packete--;
+    let packetminus="Paket ausgeliefert" ;
+    console.log(packetminus);
+    
+    if (packete==5) {
+         console.log("ich mache eine Pause")
+    }
+  
+}  ;
