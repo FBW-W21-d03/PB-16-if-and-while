f04a1543c2ce59b54977d8e2e6f823901ad21ac5
kawanaza <kawanbabani@gmail.com>
2021-11-22 11:21:54 +0100

    Done

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let paketen = 10;
+//let pauseZeit = 5;
+
+
+while (paketen > 0){
+     
+    console.log("Paket ausgeliefert");
+    paketen--;
+
+ if(paketen==5){
+ console.log("macht eine Pause");
+}
+}
