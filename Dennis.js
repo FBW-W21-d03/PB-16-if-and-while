ab5336eaff7501c5632a71a2a2e6287364b6a160
Dennisbullys <d.rochus@hotmail.de>
2021-11-22 11:34:55 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,24 @@
+console.log("If and While")
+
+let pakete = 10;
+while (pakete > 0) {
+      pakete--;
+      console.log("pakete ausgeliefert");
+}
+if (pakete ==5) {
+    console.log("Ich mache eine Pause");
+
+   
+   
+}
+
+
+
+
+
+
+
+
+
+
+
