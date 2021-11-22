fbac5ae83fc60b6227c4e0101154336d5845028d
sprotte <metatron1972@live.de>
2021-11-22 11:05:28 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let auto = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log ("packet ist ausgeliefert");
 for (auto = 0; auto < 5; auto++) {
-console.log('pause');
+console.log('packet ist ausgeliefert');
   }
   
+if (auto > 5) {
+} else if (auto > 10) {
+} else {
+}
